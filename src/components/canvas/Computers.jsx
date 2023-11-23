58a71d43e73ce64a74cf6f181269827c20243a7a
Japheth0214 <japarcillians@gmail,com>
2023-11-23 14:36:52 +0800

    Updated the computer.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,4 @@
-/* eslint-disable react/no-unknown-property */
-/* eslint-disable react/prop-types */
-import { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
